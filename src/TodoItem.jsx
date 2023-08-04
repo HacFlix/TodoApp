bed2163d7a8b06814602d7bc87c9bf20fcd4bfdb
Biswajit <hacflix@gmail.com>
2023-08-04 19:02:08 +0530

    edited css file

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react"
-import "./app.css"
-import {AiFillDelete} from "react-icons/ai"
+import { useState } from "react";
+import "./App.css";
+import { AiFillDelete } from "react-icons/ai";
 
-
-const TodoItem = ({text,id,deleteTodo}) => {
-    const [addClass, setAddClass] = useState("")
-    const checked = () =>{
-        if (addClass) {
-            setAddClass("")
-        }else{
-            setAddClass("strike")
-        }
+const TodoItem = ({ text, id, deleteTodo }) => {
+  const [addClass, setAddClass] = useState("");
+  const checked = () => {
+    if (addClass) {
+      setAddClass("");
+    } else {
+      setAddClass("strike");
     }
+  };
   return (
     <div className="todo-item">
-          <h3 onClick={checked} className={addClass}>{text}</h3>
-          <button onClick={() =>deleteTodo(id)} className="delete">
-            <AiFillDelete/>
-          </button>
-        </div>
-  )
-}
-export default TodoItem
+      <h3 onClick={checked} className={addClass}>
+        {text}
+      </h3>
+      <button onClick={() => deleteTodo(id)} className="delete">
+        <AiFillDelete />
+      </button>
+    </div>
+  );
+};
+export default TodoItem;
